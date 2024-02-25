f27d69c1e50e5c98e7b5f555f1407719d26066bf
wussh <andrianta.321@gmail.com>
2024-02-25 09:21:32 +0700

    for no key update

--- db/query/account.sql
+++ db/query/account.sql
@@ -12,7 +12,7 @@ WHERE id = $1 LIMIT 1;
 
 -- name: GetAccountForUpdate :one
 SELECT * FROM accounts
-WHERE id = $1 LIMIT 1 FOR UPDATE;
+WHERE id = $1 LIMIT 1 FOR NO KEY UPDATE;
 
 -- name: ListAccounts :many
 SELECT * FROM accounts
@@ -24,13 +24,11 @@ UPDATE accounts
 SET balance = $2
 WHERE id = $1
 RETURNING *;
-
 -- name: AddAccountBalance :one
 UPDATE accounts
 SET balance = balance + sqlc.arg(amount)
 WHERE id = sqlc.arg(id)
 RETURNING *;
-
 -- name: DeleteAccount :exec
 DELETE FROM accounts
 WHERE id = $1;
